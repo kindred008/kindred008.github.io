0515464edc8831e49bcb978ae0c5c5f05370d054
Kara Preston <kindredkara008@gmail.com>
2026-06-05 13:24:16 +0100

    Basic Hero Section design

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 export default function HomePage() {
   return (
@@ -10,6 +11,16 @@ export default function HomePage() {
 
 function HeroSection() {
     return (
-        <div className="h-[100vh] w-full bg-blue-400"></div>
+        <div className="flex h-[100vh] w-full bg-blue-200 items-center justify-center">
+          <div className="flex flex-col items-center min-w-[512px]">
+            <h1>Kara Preston</h1>
+            <h2>Experienced Software Engineer and Game Programmer</h2>
+            <div className="w-full border-t border-gray-400 my-4"></div>
+            <div className="flex flex-row gap-4">
+              <Link href="">About Me</Link>
+              <Link href="">Projects</Link>
+            </div>
+          </div>
+        </div>
     )
 }
