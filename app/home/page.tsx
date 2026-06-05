4e782445a23c5de20421f12bf3bb2cfb6f812c84
Kara Preston <kindredkara008@gmail.com>
2026-06-05 15:53:32 +0100

    Removed old unused code

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -9,22 +9,6 @@ export default function HomePage() {
   );
 }
 
-function HeroSectionBasic() {
-    return (
-        <div className="flex h-[100vh] w-full bg-blue-200 items-center justify-center">
-          <div className="flex flex-col items-center min-w-[512px]">
-            <h1>Kara Preston</h1>
-            <h2>Experienced Software Engineer and Game Programmer</h2>
-            <div className="w-full border-t border-gray-400 my-4"></div>
-            <div className="flex flex-row gap-4">
-              <Link href="">About Me</Link>
-              <Link href="">Projects</Link>
-            </div>
-          </div>
-        </div>
-    );
-}
-
 function HeroSection() {
   return (
     <div className="relative flex h-[100vh] bg-black overflow-hidden items-center justify-center">
