27c729c756616f3274579721558da6f835f603a1
Kara Preston <kindredkara008@gmail.com>
2026-06-05 13:32:08 +0100

    Comment sections of footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,12 +5,16 @@ export default function Footer() {
     return (
         <div className="bg-footer text-background min-h-32 w-full flex justify-center">
             <div className="flex flex-row w-full justify-around items-center p-2 max-w-[512px] max-[380px]:flex-col max-[380px]:gap-2">
+
+                {/*Details*/}
                 <div className="flex flex-col items-center gap-0 max-w-[250px]">
                     <p className="text-lg font-bold">Kara Preston</p>
                     <p className="text-sm text-background/70">
                         Hello! I am an experienced UK-based Software Engineer with 3 years of professional experience.
                     </p>
                 </div>
+
+                {/*Important links*/}
                 <div className = "flex flex-col items-center gap-1">
                     <p className="text-lg font-bold">Find me at</p>
                     <div className="flex flex-row gap-2">
